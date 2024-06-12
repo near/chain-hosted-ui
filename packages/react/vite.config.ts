2098e20bc3d877386f1a065c85f70fce75d5d630
Andy Haynes <andy@near.org>
2024-06-12 13:58:39 -0700

    include core dependencies

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: {
-          preset: ['@chain-deployed-ui/react-preset']
+          preset: ['@chain-deployed-ui/react-preset', 'react', 'react-dom']
         },
       },
     },
