43dfdf533cc9faa357bc6262dad3ad52fef591f8
pavelisnear <pavel.smolin@near.org>
2024-07-15 16:58:40 +0200

    Update to use react preset

--- packages/react-lib-example/vite.config.ts
+++ packages/react-lib-example/vite.config.ts
@@ -1,39 +1,29 @@
 import { defineConfig, type PluginOption } from 'vite'
+import { presetBundles } from '@chain-deployed-ui/presets/react';
 import react from '@vitejs/plugin-react'
 import { visualizer } from 'rollup-plugin-visualizer';
+import gzipPlugin from 'rollup-plugin-gzip';
 
-import BOSConfig from './bos.json' assert { type: 'json' };
-
-const { environments, filePathMatchers } = BOSConfig
-const { deployerAccount, fileServerUrl } = environments.testnet;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),visualizer() as PluginOption, {
+  plugins: [react(),visualizer() as PluginOption, gzipPlugin(), {
     name: 'inject-bundle-url',
     apply: 'build',
     transformIndexHtml(html ) {
-      return html.replace(/(href|src)="\/assets\//g, `$1="${fileServerUrl}/${deployerAccount}/`);
+      // TODO prefix root-level assets - this only removes the leading /
+      return html.replace(/(href|src)="\/((?:[\w\s-]+\/)*[^/]+\.(?:js|css|svg))/g, `$1="$2`);
     }
   }],
+  define: {
+    global: {},
+    process: { env: {} }
+  },
   build: {
     rollupOptions: {
       output: {
-        manualChunks: function (id) {
-          if (id.includes('ChainRoot')) {
-            return 'preset';
-          }
-
-          if (filePathMatchers.some((m) => id.includes(m))) {
-            return 'dev';
-          }
-
-          if (id.match(/\/node_modules\/[^/]+\/react/g)) {
-            const depRootIndex = id.indexOf('/react');
-            if (depRootIndex > -1) {
-              return 'react';
-            }
-          }
+        manualChunks: {
+          preset: presetBundles.react,
         },
       },
     },
