c887dd1e5f83c5e3b98a17ad3ac024005f36bb54
Andy Haynes <andy@near.org>
2024-07-26 15:19:48 -0700

    fix content paths

--- packages/presets/src/utils.ts
+++ packages/presets/src/utils.ts
@@ -1,4 +1,3 @@
-export function replaceHtmlPaths(html: string, application: string) {
-  // TODO prefix root-level assets - this only removes the leading /
-  return html.replace(/(href|src)="\/((?:[\w\s-]+\/)*[^/]+\.(?:js|css|svg))/g, `$1="${application}/$2`);
+export function transformUrl(filename: string, application: string) {
+  return `${application}/${filename}`;
 }
